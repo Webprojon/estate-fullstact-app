2969f5dcf934e53698b0c75e165d3aa0fa0acfba
Webprojon <tokhirjonkhasanov@gmail.com>
2025-06-09 23:00:05 +0200

    Navbar UI added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import Navbar from "./components/navbar/Navbar";
+import "./layout.scss";
+import HomePage from "./pages/homePage";
+
 function App() {
-  return (
-    <div>Hello World</div>
-  )
+	return (
+		<div className="layout">
+			<Navbar />
+			<HomePage />
+		</div>
+	);
 }
 
-export default App
+export default App;
