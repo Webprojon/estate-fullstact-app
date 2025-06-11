36494df2e8c0518f363f6d1ab1aa4d2c4630ffd1
Webprojon <tokhirjonkhasanov@gmail.com>
2025-06-11 09:50:42 +0200

    Home page is done

--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
 							<h2>Award Gained</h2>
 						</div>
 						<div className="box">
-							<h1>2000</h1>
+							<h1>1200+</h1>
 							<h2>Property Ready</h2>
 						</div>
 					</div>
